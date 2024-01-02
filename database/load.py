684a3b095a9b0e038c69631cfdda8386c6f45053
benbeard115 <benbeard2002@hotmail.co.uk>
2024-01-02 15:42:20 +0000

    connection added

---
+++ database/load.py
@@ -0,0 +1,70 @@
+"""Contains functions to upload scraped website data to a postgres database."""
+from os import environ
+from datetime import datetime
+
+from dotenv import load_dotenv
+from psycopg2 import connect, sql, DatabaseError, OperationalError
+
+
+def get_connection():
+    load_dotenv()
+    try:
+        conn = connect(user=environ["DB_USERNAME"],
+                       dbname=environ["DB_NAME"],
+                       password=environ["DB_PASSWORD"],
+                       host=environ["DB_IP"])
+        return conn
+
+    except (Exception, DatabaseError, OperationalError) as error:
+        print(error)
+        raise error
+
+
+def add_website(conn):
+    url_id = 2
+    at = datetime(2020, 6, 22, 19, 10, 20)
+    html = 'FAKER_HTML'
+    css = 'FAKER_CSS'
+
+    query = sql.SQL("""
+                    INSERT INTO {table} 
+                        ({fields})
+                    VALUES
+                        ({values});""").format(
+        table=sql.Identifier('page_scrape'),
+        fields=sql.SQL(',').join([
+            sql.Identifier('url_id'),
+            sql.Identifier('at'),
+            sql.Identifier('html'),
+            sql.Identifier('css')
+        ]),
+        values=sql.SQL(',').join([
+            sql.Literal(url_id),
+            sql.Literal(at),
+            sql.Literal(html),
+            sql.Literal(css)
+        ])
+    )
+
+    with conn.cursor() as cur:
+
+        cur.execute(query)
+
+        cur.execute("SELECT * FROM page_scrape;")
+        print(cur.fetchall())
+
+
+def print_page_scrape(conn):
+    query = sql.SQL("SELECT * FROM {table};").format(
+        table=sql.Identifier('page_scrape'))
+
+    with conn.cursor() as cur:
+        cur.execute(query)
+        rows = cur.fetchall()
+        print(rows)
+
+
+if __name__ == "__main__":
+    connection = get_connection()
+    # add_website(connection)
+    print_page_scrape(connection)
