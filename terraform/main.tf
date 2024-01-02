ca13b14a3df16c05720221c2ea6b4a257c4d53ee
benbeard115 <benbeard2002@hotmail.co.uk>
2024-01-02 17:38:12 +0000

    terraforming the database

--- terraform/main.tf
+++ terraform/main.tf
@@ -154,3 +154,36 @@ resource "aws_scheduler_schedule" "internet-archiver-scraper-schedule" {
         }
     }
 }
+
+
+
+
+resource "aws_security_group" "c9-internet-archiver-database-sg" {
+  name        = "c9-internet-archiver-database-sg"
+  description = "Allow inbound postgres traffic"
+  vpc_id      = var.VPC_ID
+
+  ingress {
+    description      = "Postgres access"
+    from_port        = 5432
+    to_port          = 5432
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_db_instance" "museum-db" {
+  allocated_storage             = 10
+  db_name                       = "postgres"
+  identifier                    = "c9-internet-archiver-terraform"
+  engine                        = "postgres"
+  engine_version                = "15.3"
+  instance_class                = "db.t3.micro"
+  publicly_accessible           = true 
+  performance_insights_enabled  = false
+  skip_final_snapshot           = true
+  db_subnet_group_name          = "public_subnet_group"
+  vpc_security_group_ids        = [aws_security_group.c9-internet-archiver-database-sg.id]
+  username                      = var.DB_USERNAME
+  password                      = var.DB_PASSWORD
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,12 @@
+variable "VPC_ID" {
+    type = string
+    default = "vpc-04423dbb18410aece"
+}
+
+variable "DB_USERNAME" {
+    type = string
+}
+
+variable "DB_PASSWORD" {
+    type = string
+}
