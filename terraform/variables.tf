bfb6e008cd905d4e71fef31632fb17f6417e1ef8
Oscar <trainee.oscar.borden@sigmalabs.co.uk>
2024-01-03 12:12:18 +0000

    Aligned EventBridge and task definition to web_scraper

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -10,3 +10,35 @@ variable "DB_USERNAME" {
 variable "DB_PASSWORD" {
     type = string
 }
+
+variable "DB_IP" {
+    type = number
+}
+
+variable "S3_BUCKET" {
+    type = string
+}
+
+variable "DB_PORT" {
+    type = string
+}
+
+variable "DB_NAME" {
+    type = string
+}
+
+variable "AWS_ACCESS_KEY_ID" {
+    type = string
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+    type = string
+}
+
+variable "URL_TABLE_NAME" {
+    type = string
+}
+
+variable "SCRAPE_TABLE_NAME" {
+    type = string
+}
