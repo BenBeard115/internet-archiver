af6c27ec195ed3724838b7f5b4a91024365e1874
Oscar <trainee.oscar.borden@sigmalabs.co.uk>
2024-01-08 15:20:42 +0000

    Pylinting all files other than api

--- dashboard/dashboard_functions.py
+++ dashboard/dashboard_functions.py
@@ -1,22 +1,23 @@
 """Script containing functions to make the graphs on the dashboard."""
-from datetime import datetime
 import pandas as pd
 import altair as alt
 import streamlit as st
 
 
 def make_date_radio():
+    """Makes a date radio."""
     radio = st.sidebar.radio(label='Date Filter', options=[
         'None', 'Date Range', "Singular Date"])
 
     st.sidebar.write(
-        """<style>div.row-widget.stRadio > div{flex-direction:row;}</style>""", unsafe_allow_html=True)
+        """<style>div.row-widget.stRadio > div{flex-direction:row;}</style>""",
+        unsafe_allow_html=True)
 
     return radio
 
 
 def make_date_filter(df: pd.DataFrame, radio: str):
-    """Makes a date filter"""
+    """Makes a date filter."""
     min_date = min(df['at'].dt.date)
     max_date = max(df['at'].dt.date)
 
@@ -39,13 +40,14 @@ def make_date_filter(df: pd.DataFrame, radio: str):
 
     if radio == "Date Range":
         selected_date = st.sidebar.date_input(
-            "Select a date", (min_date, max_date), key='date_selector', min_value=min_date, max_value=max_date)
+            "Select a date", (min_date, max_date), key='date_selector',
+            min_value=min_date, max_value=max_date)
 
         if len(selected_date) == 2:
-            return df[(df['at'].dt.date >= selected_date[0]) & (df['at'].dt.date <= selected_date[1])]
+            return df[(df['at'].dt.date >= selected_date[0])
+                    & (df['at'].dt.date <= selected_date[1])]
 
-        else:
-            return df[df['at'].dt.date == selected_date[0]]
+        return df[df['at'].dt.date == selected_date[0]]
 
     return df
 
@@ -106,12 +108,10 @@ def make_archive_searchbar(data: pd.DataFrame):
 
         # Grammar on singular/plural
         if df_result_search.shape[0] == 1:
-            st.sidebar.write("Found {} Archived Record".format(
-                str(df_result_search.shape[0])))
+            st.sidebar.write(f"Found {str(df_result_search.shape[0])} Archived Record!")
 
         else:
-            st.sidebar.write("Found {} Archived Records".format(
-                str(df_result_search.shape[0])))
+            st.sidebar.write(f"Found {str(df_result_search.shape[0])} Archived Records!")
 
         return df_result_search
     return data
@@ -126,7 +126,7 @@ def make_popular_archives_bar(data: pd.DataFrame):
         name='Count').sort_values(['Count'], ascending=False).head(5)
 
     archives = alt.Chart(data).mark_bar().encode(y=alt.Y("Count").title("Archive Count"),
-                                                 x=alt.X("url_alias").title(
-                                                     "Website").sort("-y")).configure_bar(color="#d15353")
+                                                 x=alt.X("url_alias").title("Website").
+                                                sort("-y")).configure_bar(color="#d15353")
 
     st.altair_chart(archives, use_container_width=True)
