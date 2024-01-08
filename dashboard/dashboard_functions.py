731ce2053318af2eabd42cd1762db5c9d80f4095
benbeard115 <benbeard2002@hotmail.co.uk>
2024-01-08 15:04:48 +0000

    Editing dashboard for new schema

--- dashboard/dashboard_functions.py
+++ dashboard/dashboard_functions.py
@@ -62,7 +62,7 @@ def make_hourly_archive_tracker_line(data: pd.DataFrame):
 
 
 def make_daily_archive_tracker_line(data: pd.DataFrame):
-    """Makes an daily archive tracker."""
+    """Makes a daily archive tracker."""
     st.subheader("Total Websites Archived")
 
     archived = alt.Chart(data).mark_line().encode(
